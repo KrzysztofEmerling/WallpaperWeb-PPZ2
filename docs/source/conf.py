dc55c1f736c78d579c3fb3890168bbf7b6f5f8c4
Mr Chamster <s.duda03@gmail.com>
2025-11-09 23:56:03 +0100

    Dodanie Sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'WallpaperWeb'
+copyright = '2025, Agnieszka Głowacka, Anastasiya Dorosh, Martyna Trębacz, Anna Waleczek, Oliwia Skucha, Jakub Rogoża, Krzysztof Emerling, Szymon Duda'
+author = 'Agnieszka Głowacka, Anastasiya Dorosh, Martyna Trębacz, Anna Waleczek, Oliwia Skucha, Jakub Rogoża, Krzysztof Emerling, Szymon Duda'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # dla Pythona
+    'sphinx_js',           # dla JS
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
