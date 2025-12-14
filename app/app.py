8624ba762c6ccd6308f40d35fa62cc6870730b01
Martyna Trebacz <martyna.trebacz1@gmail.com>
2025-12-14 16:56:49 +0100

    Implementacja Babelu na stronie.

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
 import flask as f
 from routes import routes
+from flask_babel import Babel
 
 app = f.Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'pl', 'ru']
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+
+babel = Babel(app)
 app.register_blueprint(routes)
+
+def get_locale():
+    lang = f.request.view_args.get('lang', None)
+    if lang in app.config['BABEL_SUPPORTED_LOCALES']:
+        return lang
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+babel.init_app(app, locale_selector=get_locale)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 import flask as f
+from flask_babel import gettext as _
 
 routes = f.Blueprint('routes', __name__)
 
-@routes.route('/')
-def index():
-    return f.render_template('index.html')
+@routes.route('/', defaults={'lang': None})
+@routes.route('/<lang>')
+def index(lang):
+    lang = f.request.view_args.get(lang, f.current_app.config['BABEL_DEFAULT_LOCALE'])
+    if lang not in f.current_app.config['BABEL_SUPPORTED_LOCALES']:
+        return f.redirect(f.url_for('routes.index', lang=f.current_app.config['BABEL_DEFAULT_LOCALE']))
+    return f.render_template('index.html', lang=lang)
+
+@routes.route('/change_lang/<new_lang>')
+def change_lang(new_lang):
+    if new_lang not in f.current_app.config['BABEL_SUPPORTED_LOCALES']:
+        new_lang = f.current_app.config['BABEL_DEFAULT_LOCALE']
+    return f.redirect(f.url_for('routes.index', lang=new_lang))
